c0819877871397a87f728a6c4df46e95ba3fbff7
Javier Gómez-Arrue <31278551+jgarrue@users.noreply.github.com>
2018-08-20 07:13:17 +0200

    Add files via upload

---
+++ server.R
@@ -0,0 +1,57 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+
+#Carga de datos
+get(load(url("https://github.com/jgarrue/EuASTA/blob/master/EuroAirportTraffic.rda?raw=true")))
+
+shinyServer(function(input, output) {
+
+  output$plot <- renderPlot({
+    
+    #Variables
+    months_ES <- c('Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre')
+    months_trans <- c(1:12)
+    names(months_trans) <- months_ES
+    targetstate   <- input$targetstate
+    targetairport <- input$targetairport
+    targetyear1   <- input$targetyear1
+    targetyear2   <- input$targetyear2
+    targetmonth1  <- months_trans[input$targetmonth1]
+    targetmonth2  <- months_trans[input$targetmonth2]
+    targetgraph   <- input$targetgraph
+    MaxDays <- c(31,28,31,30,31,30,31,31,30,31,30,31)
+    targetdate1   <- as.POSIXct.Date(as.Date(paste(targetyear1,targetmonth1,'01',sep='-')),tz='CET')
+    targetdate2   <- as.POSIXct.Date(as.Date(paste(targetyear2,targetmonth2,MaxDays[targetmonth2],sep='-')),tz='CET')
+
+    if (targetstate == 'Todos' & targetairport == 'Todos') {
+      by_countries <- filter(Airport_Traffic,FLT_DATE >= targetdate1,FLT_DATE <= targetdate2)%>%group_by(STATE_NAME)%>%summarise(DEPS=sum(FLT_DEP_1),ARR=sum(FLT_ARR_1))%>%select(STATE_NAME,DEPS,ARR)%>%arrange(desc(DEPS+ARR))
+      ggplot(by_countries,aes(x=reorder(STATE_NAME,DEPS+ARR),y=DEPS+ARR)) + geom_col(color="#005824",fill="#41ae76") + theme(axis.text.x=element_text(angle=0, size=8)) + coord_flip() + labs(x='Países',y='Vuelos Totales',title=paste('Tráfico en los aeropuertos europeos entre',targetyear1,'y',targetyear2,sep=' ')) + scale_y_continuous(expand = c(0, 0))
+    }
+    else if (targetstate != 'Todos' & targetairport == 'Todos') {
+      if (input$targetStateOptions == 'Vuelos totales agregados') {
+          by_evolution <- filter(Airport_Traffic,STATE_NAME==targetstate,FLT_DATE >= targetdate1,FLT_DATE <= targetdate2)%>%group_by(FLT_DATE)%>%summarise(DEPS=sum(FLT_DEP_1),ARR=sum(FLT_ARR_1))
+          ggplot(by_evolution,aes(x=FLT_DATE,y=DEPS+ARR)) + geom_point(color="#41ae76") + geom_smooth(color="#084594") + theme(axis.text.x=element_text(angle=0, size=8)) + labs(x='Tiempo',y='Vuelos Totales',title=paste('Tráfico agregado de los aeropuertos de',targetstate,sep=' ')) + scale_y_continuous(expand = c(0, 0))
+      }
+      else {
+          by_airport <- filter(Airport_Traffic,STATE_NAME==targetstate,FLT_DATE >= targetdate1,FLT_DATE <= targetdate2)%>%group_by(APT_NAME)%>%summarise(DEPS=sum(FLT_DEP_1),ARR=sum(FLT_ARR_1))%>%select(APT_NAME,DEPS,ARR)
+          ggplot(by_airport,aes(x=reorder(APT_NAME,DEPS+ARR),y=DEPS+ARR)) + geom_col(color="#005824",fill="#41ae76") + theme(axis.text.x=element_text(angle=0, size=8)) + coord_flip() + labs(x='Países',y='Vuelos Totales',title='Tráfico en los aeropuertos') + scale_y_continuous(expand = c(0, 0))
+      }
+
+    }
+    else if (targetairport != 'Todos') {
+      if (input$targetAirportOptions == 'Gráfico de vuelos en un periodo') {
+          by_interval <- filter(Airport_Traffic,APT_NAME==targetairport,FLT_DATE >= targetdate1,FLT_DATE <= targetdate2)
+          ggplot(by_interval,aes(x=FLT_DATE,y=FLT_DEP_1+FLT_ARR_1)) + geom_point(color="#41ae76") + geom_smooth(color="#084594") + labs(x='Tiempo',y='Vuelos',title=paste('Tráfico en el aeropuerto de',targetairport,sep=' '))
+      }
+      else {
+          by_aggregate <- filter(Airport_Traffic,APT_NAME==targetairport,YEAR>=targetyear1,YEAR<=targetyear2)%>%group_by(MONTH_NUM)
+          ggplot(by_aggregate,aes(x=MONTH_NUM,y=FLT_DEP_1+FLT_ARR_1)) + geom_boxplot(color="#005824",fill="#41ae76") + labs(x='Tiempo',y='Vuelos',title=paste('Tráfico agregado por meses en el aeropuerto de',targetairport,'( entre',targetyear1,'y',targetyear2,')',sep=' '))
+      }
+    }
+    else {}
+    
+  })
+
+  
+})
---
+++ ui.R
@@ -0,0 +1,87 @@
+#
+# Ejemplo de app shiny para master UNED Big Data
+# Módulo visualización avanzada
+#
+# Ejemplo para ejercicio de fin de módulo
+#
+# Basado en parte en el ejemplo de Joe Cheng
+# https://gist.github.com/jcheng5/3239667
+# Basado en parte en el ejemplo de Pedro Concejero
+# https://gist.github.com/pedroconcejero
+
+library(shiny)
+library(ggplot2)
+library(dplyr)
+
+#Datos
+get(load(url("https://github.com/jgarrue/EuASTA/blob/master/EuroAirportTraffic.rda?raw=true")))
+#Listas
+countries <- c('Todos',levels(as.factor(Airport_Traffic$STATE_NAME)))
+airports <- c('Todos',levels(as.factor(Airport_Traffic$APT_NAME)))
+years <- levels(as.factor(Airport_Traffic$YEAR))
+months_ES <- c('Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre')
+graphs_ES <- c('Comparativa entre países','Evolución de vuelos en un país','Comparativa entre aeropuertos de un país','Análisis de un aeropuerto','Análisis de un aeropuerto (agregado por meses)')
+
+
+shinyUI(
+  navbarPage("European Airport Space Traffic Analysis",
+                   tabPanel("Descripción del trabajo",
+                            mainPanel(
+                              h1("European Airport Space Traffic Analysis", align = "center"),
+                              h2("Estudio del tráfico aéreo en los aeropuertos europeos", align = "center"),
+                              h2("autor: Javier Gómez-Arrue Azpiazu", align = "center"),
+                              h3("Datos obtenidos de la página del repositorio Pan-Europeo de desempeño de Seguridad en Navegación Aérea de la agencia EUROCONTROL",align = "center"),
+                              h3("http://ansperformance.eu/data/performancearea/",align = "center"),
+                              p("Mediante ésta sencilla aplicación puede analizarse el tráfico aéreo de los distintos aeropuertos europeos."),
+                              p("La primera pestaña (esta) describe el contenido y el autor."),
+                              p("La segunda pestaña presenta una serie de gráficos interactivos en los que puede explorarse el tráfico de los distintos aeropuertos europeos entre 2014 y julio del 2018."),
+                              p("Pueden identificarse los países con mayor número de vuelos en europa, que corresponden a Reino Unido, Alemania, España y Francia."),
+                              p("Mediante la modificación del inicio y final del periodo revisado, puede verse como se ha ido modificando éste orden."),
+                              p("Puede observarse como España ha adelantado a Francia, separándose de ella en el último año. Y cómo el Reino Unido ha desbancado a Alemania como principal 'HUB' europeo."),
+                              p("También llama la atención la distribución estacional del tráfico de los aeropuertos más grandes, en los que se observa un acusado descenso en invierno frente a los vuelos en verano."),
+                              p("Como curiosidad en el caso de España, podemos observar que casi todos los aeropuertos principales son de poblaciones turísticas en los que hay un fuerte ascenso de tráfico en verano. Sin embargo, en el caso concreto de Madrid, Barcelona y Bilbao, a pesar de existir un aumento del tráfico en verano, también se observa un leve descenso en Agosto, motivado quizá por no ser destinos de costa. Este descenso es más acusado en Madrid y Bilbao, en los que el tráfico de agosto está por debajo del tráfico de julio y septiembre."),
+                              p("(Nota:Tras un análisis inicial de los datos, se han agregado las salidas y las llegadas al no existir prácticamente diferencia entre una y otra.)")
+                            )),
+                   tabPanel("Funcionamiento diario del aeropuerto",
+                            sidebarPanel(
+                              
+                                #selectInput('targetgraph','Indica la gráfica que quieres', graphs_ES),
+                                selectInput('targetstate','Elige qué país quieres analizar', countries, countries[1]),
+                                conditionalPanel(
+                                  condition = "input.targetstate != 'Todos'",
+                                  radioButtons('targetStateOptions','Elige qué gráfica quieres ver para este país',c('Vuelos totales agregados','Comparativa de aeropuertos'))
+                                ),
+                                selectInput('targetairport','Elige qué aeropuerto quieres analizar', airports, airports[1]),
+#                                conditionalPanel(
+#                                  condition = "input.targetstate == 'Todos'",
+#                                  OnDemandAirports <- c('Todos',levels(as.factor(Airport_Traffic$APT_NAME))),
+#                                  selectInput('targetOnDemandAirport','Elige qué aeropuerto quieres analizar',OnDemandAirports,OnDemandAirports[1])
+#                                ),
+#                                conditionalPanel(
+#                                  condition = "input.targetstate == 'Todos'",
+#                                  OnDemandAirports <- c('Todos',levels(as.factor(filter(Airport_Traffic,STATE_NAME==input$targetstate)%>%select(APT_NAME) ))),
+#                                  selectInput('targetOnDemandAirport','Elige qué aeropuerto quieres analizar',OnDemandAirports,OnDemandAirports[1])
+#                                ),
+                                conditionalPanel(
+                                  condition = "input.targetairport != 'Todos'",
+                                  radioButtons('targetAirportOptions','Elige qué gráfica quieres ver para este aeropuerto',c('Gráfico de vuelos en un periodo','Agregado por meses'))
+                                ),
+                                selectInput('targetmonth1','Indica el inicio del periodo a analizar', months_ES, months_ES[1]),
+                                selectInput('targetyear1',NULL, choices=years, selected=years[1]),
+                                selectInput('targetmonth2','Indica el final del periodo a analizar', months_ES, months_ES[12]),
+                                selectInput('targetyear2',NULL, choices=years, selected=tail(years, n=1))
+                              
+                            ),
+                            
+                            mainPanel(
+                              plotOutput('plot',
+                                         height=500)
+                              
+                            )
+                   ),
+                   tabPanel("Retrasos",
+                            h1("Actualmente en proceso de análisis de los datos", align = "center")
+                   )
+  
+))
+
